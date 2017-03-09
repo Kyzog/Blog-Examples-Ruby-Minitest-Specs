5a856adeafefeabaf093fedd16c565cdc0f0ccfa
Kyle Herzog <kyle.herzog@kyzog.com>
2017-03-08 20:30:33 -0700

    initial bundle gem command structure

---
+++ lib/Ruby/Minitest/Specs.rb
@@ -0,0 +1,9 @@
+require "Ruby/Minitest/Specs/version"
+
+module Ruby
+  module Minitest
+    module Specs
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/Ruby/Minitest/Specs/version.rb
@@ -0,0 +1,7 @@
+module Ruby
+  module Minitest
+    module Specs
+      VERSION = "0.1.0"
+    end
+  end
+end
